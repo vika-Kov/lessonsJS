36009be2af83eb6008f90b36c67dd7a7041a64a1
repo <dragondark48@gmail.com>
2021-02-20 23:57:05 +0300

    правки1

--- lesson_1.js
+++ lesson_1.js
@@ -1,8 +1,8 @@
 
-let tempC = +prompt ("Введите градусы по Цельсию")
-let tempF = (9 / 5) * tempC + 32
-alert ('Температура в градусах по Фаренгейту равна ' + tempF)
+let tempC = +prompt ("Введите градусы по Цельсию");
+let tempF = (9 / 5) * tempC + 32;
+alert ('Температура в градусах по Фаренгейту равна ' + tempF);
 
-let name = 'Василий'
-let admin = 'Василий'
-console.log(admin)
+let name = 'Василий';
+let admin = 'Василий';
+console.log(admin);
